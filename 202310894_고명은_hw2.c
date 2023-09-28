6009fc8597968da2f33c3553e94e2250271a2960
me-sienna <144670217+me-sienna@users.noreply.github.com>
2023-09-28 15:46:16 +0900

    3주차 과제 업로드

---
+++ 202310894_고명은_hw2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main(void) 
+{
+    double kilometers;
+    printf("Please enter kilometers: ");
+    scanf_s("%lf", &kilometers);
+
+    double miles = kilometers / 1.609;
+
+    printf("%.1lf km is equal to %.1lf miles\n", kilometers, miles);
+    return 0;
+}
